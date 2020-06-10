131ebba10e3c44e5307626c803f626765d007558
Chen Liang <cliang86@gmail.com>
2020-06-10 15:34:59 +0100

    Rust hello world bin and library

---
+++ helloworld/target/rls/debug/deps/helloworld-59679c4d7a0bb731.d
@@ -0,0 +1,5 @@
+/Users/cliang/projects/learning_rust/helloworld/target/rls/debug/deps/helloworld-59679c4d7a0bb731.rmeta: src/main.rs
+
+/Users/cliang/projects/learning_rust/helloworld/target/rls/debug/deps/helloworld-59679c4d7a0bb731.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/rls/debug/deps/helloworld-5a5ff06ac6209d05.d
@@ -0,0 +1,5 @@
+/Users/cliang/projects/learning_rust/helloworld/target/rls/debug/deps/helloworld-5a5ff06ac6209d05.rmeta: src/main.rs
+
+/Users/cliang/projects/learning_rust/helloworld/target/rls/debug/deps/helloworld-5a5ff06ac6209d05.d: src/main.rs
+
+src/main.rs:
