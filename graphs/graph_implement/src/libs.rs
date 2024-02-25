065691f8d46f0944c1bfc97a76595647b3e79df9
Chen Liang <chen.liang@ie.ibm.com>
2024-02-25 16:42:23 +0000

    fixed some bugs

--- graphs/graph_implement/src/libs.rs
+++ graphs/graph_implement/src/libs.rs
@@ -18,14 +18,14 @@ pub trait Graph {
     fn add_node(&mut self, node: &str) -> bool {
         match self.adjacency_matrix().get(node) {
             None => {
-                self.adjacency_matrix().insert(*node).to_string(), Vec::new());
+                self.adjacency_matrix().insert((*node).to_string(), Vec::new());
                 true
-            }
-            _ => false
+            },
+            _ => false,
         }
     }
 
-    fn add_edge(&mut self, edge: *&str, &str, i32)) {
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
         self.add_node(edge.0);
         self.add_node(edge.1);
 
@@ -49,7 +49,7 @@ impl Graph for DirectedGraph {
         }
     }
 
-    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)> {
+    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
         &mut self.adjacency_matrix
     }
 }
@@ -59,5 +59,21 @@ impl Graph for UndirectedGraph {
         UndirectedGraph {
         adjacency_matrix: HashMap::new(),
         }
+    }
+    fn adjacency_matrix(&mut self) -> &mut HashMap<String, Vec<(String, i32)>> {
+        &mut self.adjacency_matrix
+    }
+
+    fn add_edge(&mut self, edge: (&str, &str, i32)) {
+        self.add_node(edge.0);
+        self.add_node(edge.1);
+
+        self.adjacency_matrix.entry(edge.0.to_string()).and_modify(|e| {
+            e.push((edge.1.to_string(), edge.2))
+        });
+
+        self.adjacency_matrix.entry(edge.1.to_string()).and_modify(|e| {
+            e.push((edge.0.to_string(), edge.2))
+        });
     }
 }
